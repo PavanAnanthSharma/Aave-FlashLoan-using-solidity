9e082f58b3a188c8a490802d5b606f0f9bc1d4a8
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2021-10-05 23:13:28 +0530

    Update Withdrawable.sol

--- FlashloanContracts/Withdrawable.sol
+++ FlashloanContracts/Withdrawable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.6.6 <= 0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/token/ERC20/SafeERC20.sol";
