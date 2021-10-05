a60f18d69184830a2b010869ad62602ee9dba236
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2021-10-05 23:13:05 +0530

    Update ILendingPoolAddressesProvider.sol

--- FlashloanContracts/ILendingPoolAddressesProvider.sol
+++ FlashloanContracts/ILendingPoolAddressesProvider.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.6;
 /**
     @title ILendingPoolAddressesProvider interface
     @notice provides the interface to fetch the LendingPoolCore address
- */
+ */ 
 
 interface ILendingPoolAddressesProviderV1 {
     function getLendingPoolCore() external view returns (address payable);
