67e3a4298c9a37350a855081a909e9cd786cddfd
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2021-10-05 23:13:11 +0530

    Update ILendingPoolAddressesProvider.sol

--- FlashloanContracts/ILendingPoolAddressesProvider.sol
+++ FlashloanContracts/ILendingPoolAddressesProvider.sol
@@ -9,3 +9,4 @@ interface ILendingPoolAddressesProviderV1 {
     function getLendingPoolCore() external view returns (address payable);
     function getLendingPool() external view returns (address);
 }
+  
